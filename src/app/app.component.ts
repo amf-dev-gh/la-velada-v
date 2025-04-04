16967298f1483199d2f8d869f1b5c807da6f7e7c
Andres Fernandez <andres94.dev@gmail.com>
2025-04-04 18:39:03 +0200

    subiendo proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SponsorsComponent } from './components/sponsors/sponsors.component';
+import { FIGHTERS } from './consts/fighters';
+import { Fighters } from './types/fighters';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SponsorsComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'la-velada-v';
+  f: Fighters = FIGHTERS[0]
 }
