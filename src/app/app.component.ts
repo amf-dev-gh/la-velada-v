542eaad50ef981bf924a1c02d2a8c58bbfe6ae62
Andres Fernandez <andres94.dev@gmail.com>
2025-04-04 21:48:36 +0200

    agregado el componente hero al proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { FooterComponent } from './components/footer/footer.component';
 import { SponsorsComponent } from './components/sponsors/sponsors.component';
 import { FIGHTERS } from './consts/fighters';
 import { Fighters } from './types/fighters';
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SponsorsComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, SponsorsComponent, FooterComponent, HeroComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
