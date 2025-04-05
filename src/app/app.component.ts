dbfe00dae36ddfb35fb7549cea12ddafaecaf2a8
Andres Fernandez <andres94.dev@gmail.com>
2025-04-05 12:43:14 +0200

    elimino el header y la variable de prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SponsorsComponent } from './components/sponsors/sponsors.component';
-import { FIGHTERS } from './consts/fighters';
-import { Fighters } from './types/fighters';
 import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SponsorsComponent, FooterComponent, HeroComponent],
+  imports: [RouterOutlet, SponsorsComponent, FooterComponent, HeroComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  f: Fighters = FIGHTERS[0]
+
 }
