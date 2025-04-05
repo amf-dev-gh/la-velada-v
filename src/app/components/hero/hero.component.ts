3dadab1b51ad3a1ff86b114272a3276fe8da34f0
Andres Fernandez <andres94.dev@gmail.com>
2025-04-05 16:05:21 +0200

    inertadas filas de peleadores y modificacion del logo central

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -3,16 +3,21 @@ import { FIGHTERS } from '../../consts/fighters';
 import { Fighter } from '../../types/fighters';
 import { RouterLink } from '@angular/router';
 import { FighterCardComponent } from '../fighter-card/fighter-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
-  imports: [FighterCardComponent],
+  imports: [FighterCardComponent, CommonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
 
-  fightersRow1:Fighter[] = FIGHTERS.slice(0,6);
-  fightersRow2:Fighter[] = FIGHTERS.slice(6);
+  fightersFirstRow:Fighter[] = FIGHTERS.slice(0,6);
+
+  leftFirtRow = this.fightersFirstRow.slice(0,3)
+  rightFirtRow = this.fightersFirstRow.slice(3)
+
+  fightersSecondRow:Fighter[] = FIGHTERS.slice(6);
 
 }
