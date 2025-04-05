6ebe942fa8f9ca437b8a7ebd4d4ebfd8ec6803cc
Andres Fernandez <andres94.dev@gmail.com>
2025-04-05 20:58:16 +0200

    agrego prioridad de carga al logo y hover de luchadores y oponentes sincronizado

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { FIGHTERS } from '../../consts/fighters';
 import { Fighter } from '../../types/fighters';
-import { RouterLink } from '@angular/router';
 import { FighterCardComponent } from '../fighter-card/fighter-card.component';
 import { CommonModule } from '@angular/common';
+import { COMBATS } from '../../consts/combats';
 
 @Component({
   selector: 'app-hero',
@@ -13,26 +13,47 @@ import { CommonModule } from '@angular/common';
 })
 export class HeroComponent {
 
-  allFighters:Fighter[] = FIGHTERS;
+  allFighters: Fighter[] = FIGHTERS;
 
-  fightersFirstRow:Fighter[] = FIGHTERS.slice(0,6);
-
-  leftFirtRow = this.fightersFirstRow.slice(0,3)
+  fightersFirstRow: Fighter[] = FIGHTERS.slice(0, 6);
+  leftFirtRow = this.fightersFirstRow.slice(0, 3)
   rightFirtRow = this.fightersFirstRow.slice(3)
+  fightersSecondRow: Fighter[] = FIGHTERS.slice(6);
 
-  fightersSecondRow:Fighter[] = FIGHTERS.slice(6);
+  visibleFighter: string = '';
+  showLanging: boolean = true;
+  hoveredFighter: boolean = false;
 
-  visibleFighter:string = '';
-  showLanging:boolean = true;
-
-  showFighter(fighterId:string){
+  showFighter(fighterId: string) {
     this.showLanging = false
     this.visibleFighter = fighterId;
   }
 
-  showLandig(){
+  showLandig() {
     this.showLanging = true
     this.visibleFighter = ''
+  }
+
+  hovered(fighterId: string): string {
+    if (this.visibleFighter === ''){
+      return 'opacity-100 transition-all duration-300'
+    }
+    if (this.visibleFighter === fighterId || this.visibleFighter === this.oponent(fighterId)) {
+      return 'opacity-100 duration-300 scale-110 transition-all'
+    }
+      return 'opacity-60 transition-all delay-100'
+  }
+
+  private oponent(fighterId:string):string{
+    for(const combat of COMBATS) {
+      if (combat.fighters[0] === fighterId) {
+        return combat.fighters[1]
+      }
+      if (combat.fighters[1] === fighterId) {
+        return combat.fighters[0]
+      }
+    };
+    return fighterId
   }
 
 }
