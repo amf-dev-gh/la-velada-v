683aa73ff4513f3460b9d72fc1e9ca1ab0dd507b
Andres Fernandez <andres94.dev@gmail.com>
2025-04-05 13:07:22 +0200

    usando card boxeador en hero

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FIGHTERS } from '../../consts/fighters';
+import { Fighter } from '../../types/fighters';
+import { RouterLink } from '@angular/router';
+import { FighterCardComponent } from '../fighter-card/fighter-card.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [FighterCardComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+
+  fightersRow1:Fighter[] = FIGHTERS.slice(0,6);
+  fightersRow2:Fighter[] = FIGHTERS.slice(6);
 
 }
