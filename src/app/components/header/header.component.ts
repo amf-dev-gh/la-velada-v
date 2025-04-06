5fdce29d0d2d5ed48aafd1a3af7a2dcb5343e5f4
Andres Fernandez <andres94.dev@gmail.com>
2025-04-06 20:40:34 +0200

    agrego svg icon de la velada

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { IconComponent } from "../icon/icon.component";
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, IconComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
