f4da2c991580d1a730b8368e3e03419fbcecdb20
Andres Fernandez <andres94.dev@gmail.com>
2025-04-06 15:02:27 +0200

    agregadas rutas de inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,11 @@ import { LuchadorComponent } from './components/luchador/luchador.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { CombateComponent } from './components/combates/combates.component';
 import { LaPorraComponent } from './components/la-porra/la-porra.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 export const routes: Routes = [
+  {path:'', component:HeroComponent},
+  {path:'inicio', component:HeroComponent},
   {path:'luchador/:id', component:LuchadorComponent},
   {path:'la-porra', component:LaPorraComponent},
   {path:'combate/:id', component:CombateComponent},
