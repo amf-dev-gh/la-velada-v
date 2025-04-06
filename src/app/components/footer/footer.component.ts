ba44e9c62a9701b4d010f5e2e2fd3abe2c7b3a47
Andres Fernandez <andres94.dev@gmail.com>
2025-04-06 18:42:35 +0200

    implementacion de componente icono en footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { SOCIALS } from '../../consts/social';
 import { Social } from '../../types/social';
+import { IconComponent } from "../icon/icon.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [IconComponent, CommonModule],
   templateUrl: './footer.component.html'
 })
 export class FooterComponent {
 
   socials:Social[] = SOCIALS;
-  foot:string = '© 2025 La Velada del Año. Todos los derechos reservados.'
+  textFooter:string = '© 2025 La Velada del Año. Todos los derechos reservados.'
 }
