7ff91e5fb8ab477317190ab354cafa87c0ffe3bb
Andres Fernandez <andres94.dev@gmail.com>
2025-04-07 10:24:52 +0200

    creado componente de presentacion con video

---
+++ src/app/components/presentracion/presentracion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgxLiteYoutubeModule } from 'ngx-lite-video';
+
+@Component({
+  selector: 'app-presentracion',
+  imports: [NgxLiteYoutubeModule ],
+  templateUrl: './presentracion.component.html',
+  styleUrl: './presentracion.component.css'
+})
+export class PresentracionComponent {
+
+}
