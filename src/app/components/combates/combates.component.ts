827b9e00ddf968cbc7cd3570b1626d82427b4f8a
Andres Fernandez <andres94.dev@gmail.com>
2025-04-07 19:11:07 +0200

    implementando cards a paginma de combates

--- src/app/components/combates/combates.component.ts
+++ src/app/components/combates/combates.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { BackgroundComponent } from "../background/background.component";
+import { CardCombateComponent } from "./card-combate/card-combate.component";
+import { COMBATS } from '../../consts/combats';
+import { CommonModule } from '@angular/common';
+import { Combat } from '../../types/Combat';
 
 @Component({
   selector: 'app-combates',
-  imports: [BackgroundComponent],
+  imports: [BackgroundComponent, CardCombateComponent, CommonModule],
   templateUrl: './combates.component.html',
   styleUrl: './combates.component.css'
 })
 export class CombateComponent {
+
+  combats: Combat[] = COMBATS;
 
 }
