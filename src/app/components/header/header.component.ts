6e4b3ef6133f84a960a2aee0df600584fed71ea4
Andres Fernandez <andres94.dev@gmail.com>
2025-04-08 12:15:36 +0200

    creado menu expansible movile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { IconComponent } from "../icon/icon.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, IconComponent],
+  imports: [RouterLink, IconComponent, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  menuExpanded:boolean=false;
+
+  showMenu(){
+    this.menuExpanded = !this.menuExpanded;
+  }
+
+  closeMenu(){
+    this.menuExpanded = false;
+  }
 
 }
