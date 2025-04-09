34a9b02096b1945583e5b58cb1c9d50cc5e20fdf
Andres Fernandez <andres94.dev@gmail.com>
2025-04-09 09:51:16 +0200

    configuracion para que el componente luchador recargue si cambia id

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, RouteReuseStrategy } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { CustomReuseStrategy } from './custom-reuse-strategy';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),{ provide: RouteReuseStrategy, useClass: CustomReuseStrategy }, provideAnimations()]
 };
